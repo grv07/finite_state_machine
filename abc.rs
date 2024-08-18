022b3a342b5955b1b0ed47ba6d93f369c2c17727
Gaurav Tyagi <grvtyagi22@gmail.com>
2024-08-18 10:17:14 +0530

    Push an initial state and change find logic

--- abc.rs
+++ abc.rs
@@ -20,20 +20,22 @@ struct Regex {
 
 impl Regex {
     fn compile(input: &str) -> Self {
-        let mut fsm = Regex { row: vec![] };
+        let mut fsm = Regex {
+            row: vec![FsmColumn::new()],
+        };
 
         for c in input.chars() {
             match c {
                 '$' => {
                     let mut column = FsmColumn::new();
-                    column.column[LINE_END] = fsm.row.len();
+                    column.column[LINE_END] = fsm.row.len() + 1;
                     fsm.row.push(column);
                 }
 
                 '.' => {
                     let mut column = FsmColumn::new();
                     for i in 0..127 {
-                        column.column[i] = 1;
+                        column.column[i] = fsm.row.len() + 1;
                     }
 
                     fsm.row.push(column);
@@ -51,14 +53,14 @@ impl Regex {
     }
 
     fn find(&self, query: &str) -> bool {
-        let mut state: usize = 0;
+        let mut state: usize = 1;
 
         for c in query.chars() {
-            if self.row[state].column[c as usize] == 0 {
+            if state == 0 && self.row[state].column[c as usize] == 0 {
                 return false;
             }
 
-            state += 1;
+            state = self.row[state].column[c as usize];
         }
 
         if self.row[state].column[LINE_END] == 0 {
@@ -80,11 +82,13 @@ impl Regex {
 }
 
 fn main() {
-    let regex = Regex::compile("ab.$");
+    let regex_q = "ab.$";
+    let regex = Regex::compile(regex_q);
     regex.dump();
 
     let queries = vec!["ab", "abc", "abd", "abb", "hello"];
 
+    println!("Regex: {}", regex_q);
     for query in queries {
         println!("{query} => {}", regex.find(query));
     }
